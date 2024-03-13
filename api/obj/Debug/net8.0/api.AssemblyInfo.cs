c31a9bb94b20a15643a1c5abe8bf0fa6390e8731
LeBrounius <ignas.bakus77@gmail.com>
2024-03-13 10:53:20 +0200

    Pasileidau koda ant loptopo

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c7ccaaa628c95522c45b91b747c1080c8a77f2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22e993b0556691fe952165b4e12cf4050fcbd45c")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
